0bcf620ff1242f5377794edd2443affecc9fa86b
Poorva Adhikary <adhikarypoorvaraja@gmail.com>
2026-01-18 10:31:51 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Lets_Cook',
+  webDir: 'www'
+};
+
+export default config;
