cf007cf11867791d3e8e9776ebdfb874a916407c
Niladri <niladrikarmakar006@gmail.com>
2026-03-03 19:21:47 +0530

    fix: gitignore fix for env vairables

--- src/environments/environment.example.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://rwqnrtzcvpaqhsppgdwc.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3cW5ydHpjdnBhcWhzcHBnZHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NTEyOTMsImV4cCI6MjA4NzQyNzI5M30.0MFBTXkacMhL_fuvg3d-iMnVQAVoBYgsNmESc2Xa5mg',
-};
