037824d8b1d44afe40a94c022561691139ceb62c
Poorva Adhikary <adhikarypoorvaraja@gmail.com>
2026-03-03 17:26:02 +0530

    updated spelling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 //   supabaseAnonKey: (window as any).__env?.supabaseAnonKey || ''
 // };
 
-export const enviornment={
+export const environment={
   production: true,
   supabaseUrl: 'https://rwqnrtzcvpaqhsppgdwc.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3cW5ydHpjdnBhcWhzcHBnZHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NTEyOTMsImV4cCI6MjA4NzQyNzI5M30.0MFBTXkacMhL_fuvg3d-iMnVQAVoBYgsNmESc2Xa5mg',
